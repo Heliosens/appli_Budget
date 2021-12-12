e848270237252876f8bd0ad192dfaacb92dafb06
Sylvie-B <unsolo@hotmail.fr>
2021-12-12 11:37:19 +0100

    review modal

--- appModal.js
+++ appModal.js
@@ -1,5 +1,10 @@
 /**
- *
+ *  create modal window
+ *  .screen id='container',
+ *  .box id='theBox' and inner box id='innerBox'
+ *  enable to create input element and button
+ *  .inputBox id='inputModalId';
+ *  .closeBtn id='btnModalId'
  * @param screenColor
  * @param w
  * @param h
@@ -9,22 +14,20 @@
  */
 function ModalWindow (screenColor, w, h, boxColor, border){
     /**
-     * create cover screen
+     * create screen cover
      */
     this.screen = function (){
         let fullScreen = document.createElement('div');
         fullScreen.style.width = innerWidth + "px";
         fullScreen.style.height = innerHeight + "px";
         fullScreen.style.backgroundColor = screenColor;
-
         fullScreen.style.position = "absolute";
         fullScreen.style.top = "0";
         fullScreen.style.left = "0";
-
         fullScreen.style.display = "flex";
         fullScreen.style.justifyContent = "center";
         fullScreen.style.alignItems = "center";
-
+        // affect id
         fullScreen.id = 'container';
 
         document.body.appendChild(fullScreen);
@@ -36,37 +39,43 @@ function ModalWindow (screenColor, w, h, boxColor, border){
      * @param text
      */
     this.box = function (title = "", text =""){
+        // create modal window
         let theBox = document.createElement('div');
+        // style
         theBox.style.backgroundColor = boxColor;
         theBox.style.border = border;
         theBox.style.width = w;
         theBox.style.height = h;
         theBox.style.padding = "1vh";
         theBox.style.fontFamily = "sans-serif";
-        theBox.id = "theBox";
-
         theBox.style.display = "flex";
         theBox.style.flexDirection = "column";
         theBox.style.justifyContent = "space-around";
         theBox.style.alignItems = "center";
-        // append in container
-        let container = document.getElementById('container');
-        container.appendChild(theBox);
+
+        // affect id
+        theBox.id = "theBox";
+
         // create title
         let h1 = document.createElement('h1');
         h1.innerHTML = (title).toString();
         theBox.appendChild(h1);
 
-        // create inner box
+        // create inner box id='innerBox'
         let div = document.createElement('div');
-        div.id = "innerBox";
         div.style.textAlign = "center";
+        div.id = "innerBox";
         theBox.appendChild(div);
+
         // add text
         let p = document.createElement('p');
         p.innerHTML = (text).toString();
         p.style.margin = '1rem';
         div.appendChild(p);
+
+        // append in container
+        let container = document.getElementById('container');
+        container.appendChild(theBox);
     }
 
     /**
@@ -76,15 +85,20 @@ function ModalWindow (screenColor, w, h, boxColor, border){
      * @param txt
      */
     this.inputBox = function (h = '3rem', sizeTxt = '2rem', txt = ""){
+        // create label
         let labelFrame = document.createElement('label');
         labelFrame.innerHTML = txt;
         labelFrame.style.alignSelf = "left";
+        // create input
         let inputFrame = document.createElement('input');
         inputFrame.style.height = h;
         inputFrame.style.fontSize = sizeTxt;
         inputFrame.name = "inputInBox";
         inputFrame.type = "number";
-        inputFrame.id = "inputFrameId";
+
+        // affect id
+        inputFrame.id = "inputModalId";
+
         let innerBox = document.getElementById('innerBox');
         innerBox.appendChild(labelFrame);
         innerBox.appendChild(inputFrame);
@@ -96,20 +110,23 @@ function ModalWindow (screenColor, w, h, boxColor, border){
      * @param btnText
      */
     this.closeBtn = function (btnText){
-        let inputFrameId = document.getElementById("inputFrameId");
-        let theBox = document.getElementById('theBox');
         // create button
         let btn = document.createElement('button');
         btn.type = "submit";
         btn.style.fontSize = "2rem";
         btn.style.padding = ".5rem 1rem";
         btn.innerHTML = btnText;
+        btn.id = "btnFrameId";
         btn.type = "submit";
         btn.name = "inputInBox";
+
+        // get element to close
         let container = document.getElementById('container');
         btn.addEventListener('click', function (){
             container.remove();
         });
+        // get target element
+        let theBox = document.getElementById('theBox');
         theBox.appendChild(btn);
     }
 }
