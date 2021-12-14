6114b1c7100d033bfa395e80e6f6a8ab7381f679
Sylvie-B <unsolo@hotmail.fr>
2021-12-14 19:00:33 +0100

    according js to match css

--- appBudget.js
+++ appBudget.js
@@ -2,15 +2,22 @@
 let main = document.querySelector('main');
 main.style.height = innerHeight + "px";
 
+let side1 = document.createElement('div');
+let side2 = document.createElement('div');
+side1.id = "side";
+side2.id = "side";
+main.appendChild(side1);
+main.appendChild(side2);
 
-let place = new BudgetTable(main);
-place.titleTxt("Budget mensuel", "title");
+let place = new BudgetTable();
+
+place.titleTxt(main,"Budget mensuel", "title");
 
 // create differents section
-place.budgetSection("outlay", "Dépenses");
-place.budgetSection("income", "Recettes");
-place.budgetSection("saving", "Epargne");
-place.budgetSection("result", "Résultat")
+place.budgetSection(side1,"outlay", "Dépenses");
+place.budgetSection(side2,"income", "Recettes");
+place.budgetSection(side2,"saving", "Epargne");
+place.budgetSection(side2,"result", "Résultat")
 
 // create outlays fixe
 place.budgetType("outlay", "Dépenses fixes", "Loyer et Charges", "Remboursement de crédits",
@@ -44,11 +51,11 @@ for (let item of arrayLink){
         e.preventDefault();
 
         // invoke modal window
-        let frame = new ModalWindow(main,'rgba(173, 216, 230, 0.8)', '50%', '50vh', 'white', '3px double black');
+        let frame = new ModalWindow(main,'rgba(173, 216, 230, 0.8)', '50%', '40vh', 'white', '3px double black');
         frame.screen();
         frame.box("Poste de dépense",item.innerHTML);
-        frame.inputBox("number","2rem", "2rem", "montant ");
-        frame.closeBtn('valider', '1rem');
+        frame.inputBox("number","2rem", "1.5rem", "", true);
+        frame.closeBtn('valider');
         let inputModal = document.getElementById("inputModalId");
         let btn = document.getElementById("theBox").querySelector('button');
 
